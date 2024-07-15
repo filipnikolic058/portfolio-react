42c78bf8f7f1b96b86cfb335be4e41b4c38060df
dzonizivotinja <filipnikolic058@gmail.com>
2024-07-15 14:49:30 +0200

    responsive fix

--- src/Components/Technologies/Technologies.jsx
+++ src/Components/Technologies/Technologies.jsx
@@ -2,17 +2,17 @@ import styles from './tech.module.css'
 import Title from '../Title/Title'
 import TechCard from '../TechCard/TechCard'
 
-export default function Technologies(){
+export default function Technologies() {
 
     return (
         <>
             <div className='section container'>
-                <Title isH3={true} text = "Technologies I use"/>
+                <Title isH3={true} text="Technologies I use" />
                 <div className={styles.grid}>
-                    <TechCard imgUrl = './react.png' heading = 'React' text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut'/>
-                    <TechCard imgUrl = './nodejs.png' heading = 'NodeJS' text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut'/>
-                    <TechCard imgUrl = './express.png' heading = 'ExpressJS' text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut'/>
-                    <TechCard imgUrl = './mysql.png' heading = 'SQL based DB' text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut'/>
+                    <TechCard imgUrl='./react.png' heading='React' text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut' />
+                    <TechCard imgUrl='./nodejs.png' heading='NodeJS' text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut' />
+                    <TechCard imgUrl='./express.png' heading='ExpressJS' text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut' />
+                    <TechCard imgUrl='./mysql.png' heading='SQL based DB' text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut' />
                 </div>
             </div>
         </>
