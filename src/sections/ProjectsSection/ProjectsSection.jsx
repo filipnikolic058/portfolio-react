98b4cdb1b9bcc6afe4f5c4178bb2cc1c1d653fc7
filipnikolic058 <filipnikolic058@gmail.com>
2025-03-03 16:59:45 +0100

    test deploy

--- src/sections/ProjectsSection/ProjectsSection.jsx
+++ src/sections/ProjectsSection/ProjectsSection.jsx
@@ -4,7 +4,7 @@ import styles from "./projects.module.css"
 export default function ProjectsSection() {
     return (
         <div className={styles.container}>
-            <Link><div className="main-button">Back to Home</div></Link>
+            <Link to="/"><div className="main-button">Back to Home</div></Link>
             <h2>in construction...</h2>
         </div>
     )
