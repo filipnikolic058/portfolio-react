2a911e952680d41f43a45a8f4d91c6a724b40d20
filipnikolic058 <filipnikolic058@gmail.com>
2025-04-08 18:26:37 +0200

    animated lines background, small fixes

--- src/sections/My Work/MyWork.jsx
+++ src/sections/My Work/MyWork.jsx
@@ -2,7 +2,6 @@ import styles from './mywork.module.css'
 import WorkExample from '../../Components/WorkExample/WorkExample'
 import Title from '../../Components/Title/Title'
 import Slider from 'react-slick';
-import SlideItem from '../../Components/SlideItem/SlideItem';
 
 export default function MyWork() {
     let settings = {
@@ -26,38 +25,36 @@ export default function MyWork() {
     ];
 
     return (
-        <>
-            <div className="section">
-                <div id='my-work' className="container">
-                    <Title isH3={false} text="Some of my Work" />
-                    <div className={styles.grid}>
-                        <WorkExample link='https://nodexai.tech' imgUrl='/nodexai.png' heading='NodexAI' text='Landing page for crypto Dapp. It was developed in NextJS with Typescript and TailwindCSS for good SEO and performance.' />
-                        <WorkExample link='https://konkab.com' imgUrl='/konkab-min.jpg' heading='Konkab' text='The primary goal of this website was to make this business look more professional and provide more information about it. Design was not a priority so it was made simple but functional. Development was done using Webflow and some custom code.' />
-                        <WorkExample link='https://tepihservispetrovic.rs' imgUrl='/tepihservis-min.jpg' heading='Tepih Servis Petrovic' text='The site was developed in Webflow and designed in Figma for a local client. I have integrated slick slider, Google Maps, analytics, and tracking SEO through Google Search Console. I manage the domain, hosting, and mail accounts through Cpanel.' />
-                        <WorkExample link='https://secure.bigbluhammer.com' imgUrl='/bluhammer-min.jpg' heading='Big Blu Hammer' text="This website was not developed by me, but I manage its UI and UX, fix bugs, manage its CMS etc." />
-                        <WorkExample link='https://secure.oakhilliron.com' imgUrl='/oakhilliron-min.jpg' heading='Oak Hill Iron' text="Same as with Blu, but I didn't manage its CMS that much, only UI and UX." />
+        <section className="section">
+            <div id='my-work' className="container">
+                <Title isH3={false} text="Some of my Work" />
+                <div className={styles.grid}>
+                    <WorkExample link='https://nodexai.tech' imgUrl='/nodexai.png' heading='NodexAI' text='Landing page for crypto Dapp. It was developed in NextJS with Typescript and TailwindCSS for good SEO and performance.' />
+                    <WorkExample link='https://konkab.com' imgUrl='/konkab-min.jpg' heading='Konkab' text='The primary goal of this website was to make this business look more professional and provide more information about it. Design was not a priority so it was made simple but functional. Development was done using Webflow and some custom code.' />
+                    <WorkExample link='https://tepihservispetrovic.rs' imgUrl='/tepihservis-min.jpg' heading='Tepih Servis Petrovic' text='The site was developed in Webflow and designed in Figma for a local client. I have integrated slick slider, Google Maps, analytics, and tracking SEO through Google Search Console. I manage the domain, hosting, and mail accounts through Cpanel.' />
+                    <WorkExample link='https://secure.bigbluhammer.com' imgUrl='/bluhammer-min.jpg' heading='Big Blu Hammer' text="This website was not developed by me, but I manage its UI and UX, fix bugs, manage its CMS etc." />
+                    <WorkExample link='https://secure.oakhilliron.com' imgUrl='/oakhilliron-min.jpg' heading='Oak Hill Iron' text="Same as with Blu, but I didn't manage its CMS that much, only UI and UX." />
+                </div>
+                <Title isH3={false} text="Mobile apps" />
+                <div className={styles.sliderContainer}>
+                    <div className={styles.sliderWrapper}>
+                        <Slider {...settings} className={styles.slider}>
+                            {images.map((src, index) => (
+                                <div key={index} className={styles.slide}>
+                                    <img src={src} alt={`Slide ${index + 1}`} className={styles.image} />
+                                </div>
+                            ))}
+                        </Slider>
                     </div>
-                    <Title isH3={false} text="Mobile apps" />
-                    <div className={styles.sliderContainer}>
-                        <div className={styles.sliderWrapper}>
-                            <Slider {...settings} className={styles.slider}>
-                                {images.map((src, index) => (
-                                    <div key={index} className={styles.slide}>
-                                        <img src={src} alt={`Slide ${index + 1}`} className={styles.image} />
-                                    </div>
-                                ))}
-                            </Slider>
-                        </div>
-                        <div className={styles.textWrapper}>
-                            <h2 className={styles.title}>Moj Servis</h2>
-                            <p className={styles.description}>
-                                I also do Mobile development using React Native and Expo. This app is a personal project made with Supabase. I used supabase Auth and Storage to manage users and photos.
-                            </p>
-                        </div>
+                    <div className={styles.textWrapper}>
+                        <h2 className={styles.title}>Moj Servis</h2>
+                        <p className={styles.description}>
+                            I also do Mobile development using React Native and Expo. This app is a personal project made with Supabase. I used supabase Auth and Storage to manage users and photos.
+                        </p>
                     </div>
                 </div>
             </div>
-        </>
+        </section>
     )
 }
 
--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -2,12 +2,16 @@ import ProgLangs from '../../Components/ProgLangs/ProgLangs';
 import Technologies from '../../Components/Technologies/Technologies';
 import Tools from '../../Components/Tools/Tools';
 import styles from './skills.module.css'
+import Background from '../../Components/Background/Background';
 
 export default function Skills() {
 
     return (
         <>
             <section id='skills' className={`section ${styles.background}`}>
+                <div className={styles.backgroundImage}>
+                    <Background />
+                </div>
                 <ProgLangs />
                 <Technologies />
                 <Tools />
